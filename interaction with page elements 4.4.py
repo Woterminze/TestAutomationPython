87d21745c5006b7c19d5d29e31532b041ba3c417
woterminze <qweaspot@gmail.com>
2023-01-19 16:05:07 +0300

    work with page elements (by id)

---
+++ interaction with page elements 4.4.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service('C:\\Users\\Tanya\\PycharmProjects\\TestAutomation\\chromedriver.exe')
+driver_g = webdriver.Chrome(options=options, service=g)
+base_url = 'https://www.saucedemo.com/'
+driver_g.get(base_url)
+driver_g.maximize_window()
+user_name = driver_g.find_element(By.ID, 'user-name')
+user_name.send_keys("standard_user")
+
+# time.sleep(5)
+# driver_g.close()
