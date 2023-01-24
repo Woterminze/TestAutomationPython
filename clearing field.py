942b753cc7888a76b60dccc731b71e58da25d360
woterminze <qweaspot@gmail.com>
2023-01-24 20:43:18 +0300

    clearing the contents of the field

---
+++ clearing field.py
@@ -0,0 +1,30 @@
+#
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service('C:\\Users\\Tanya\\PycharmProjects\\TestAutomation\\chromedriver.exe')
+driver_g = webdriver.Chrome(options=options, service=g)
+base_url = 'https://www.saucedemo.com/'
+driver_g.get(base_url)
+driver_g.maximize_window()
+
+login_standard_user = "standard_user"
+password_all = "secret_sauce"
+
+
+user_name = driver_g.find_element(By.XPATH, '//input[@id="user-name"]')
+user_name.send_keys(login_standard_user)
+print("Login input - success")
+time.sleep(3)
+user_name.clear() # очистка содержимого поля
+# password = driver_g.find_element(By.XPATH, '//input[@id="password"]')
+# password.send_keys(password_all)
+# print("Password input - success")
+# button_login = driver_g.find_element(By.XPATH, '//input[@id="login-button"]')
+# button_login.click()
+# print("Login button click - success")
+
