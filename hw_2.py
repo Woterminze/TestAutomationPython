3cc9af04d5a27b28768cfd9684b0778507aa88e9
Woterminze <qweaspot@gmail.com>
2023-01-31 16:48:30 +0300

    hw 2(smoke test)

--- hw_2.py
+++ hw_2.py
@@ -1,9 +1,3 @@
-# 1.Авторизоваться на сайте
-# 2.Выбрать 2 товара
-# 3.Сохранить в переменные названия и цены товаров
-# 4.Пройти весь БП, на моменте оплаты товара сверить сохраненные значения, а так же что система правильно посчитала сумму товаров (отдельно считаем сумм товаров и проверяем с тем что говорит нам система)
-# 5.Не забывайте добавлять PRINT и КОММЕНТАРИИ для лучшей читаемости кода
-
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -35,25 +29,27 @@ login_button.click()
 print("Login button click - success")
 
 # selection of products
-product_1 = driver_g.find_element(By.XPATH, "//a[@id='item_1_title_link']")
+product_1 = driver_g.find_element(By.XPATH, "//a[@id='item_0_title_link']")
 value_product_1 = product_1.text  # product 1 name
 print("Product 1 is: " + value_product_1)
 
-product_1_price = driver_g.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[3]/div[2]/div[2]/div")  # такие страшные локаторы, потому что товаров 2, а в верстке называются одинаково, пришлось копировать
+product_1_price = driver_g.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[2]/div[2]/div[2]/div")  # такие страшные локаторы, потому что товаров 2, а в верстке называются одинаково, пришлось копировать
 value_product_1_price = product_1_price.text  # product 1 price
-print("Product 1 costs: " + value_product_1)
+print("Product 1 costs: " + value_product_1_price)
+item_1 = value_product_1_price.replace('$', '')  # сохраняем числовое значение цены 1 товара
 
 select_product_1 = driver_g.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']")
 select_product_1.click()
 print("Add to cart product 1 - success")
 
-product_2 = driver_g.find_element(By.XPATH, "//a[@id='item_0_title_link']")
+product_2 = driver_g.find_element(By.XPATH, "//a[@id='item_1_title_link']")
 value_product_2 = product_2.text  # product 2 name
 print("Product 2 is: " + value_product_2)
 
-product_2_price = driver_g.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[2]/div[2]/div[2]/div")
+product_2_price = driver_g.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[3]/div[2]/div[2]/div")
 value_product_2_price = product_2_price.text  # product 2 price
-print("Product 2 costs: " + value_product_2)
+print("Product 2 costs: " + value_product_2_price)
+item_2 = value_product_2_price.replace('$', '')  # сохраняем числовое значение цены 2 товара
 
 select_product_2 = driver_g.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bike-light']")
 select_product_2.click()
@@ -65,19 +61,19 @@ cart_button.click()
 print("Go to cart - success")
 
 # cart info
-cart_product_1 = driver_g.find_element(By.XPATH, "//a[@id='item_1_title_link']")
+cart_product_1 = driver_g.find_element(By.XPATH, "//a[@id='item_0_title_link']")
 value_cart_product_1 = cart_product_1.text
 print("Product 1 in cart is: " + value_cart_product_1)
 
-cart_product_1_price = driver_g.find_element(By.XPATH, "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div")
+cart_product_1_price = driver_g.find_element(By.XPATH, "//*[@id='cart_contents_container']/div/div[1]/div[4]/div[2]/div[2]/div")
 value_cart_product_1_price = cart_product_1_price.text
 print("Costs: " + value_cart_product_1_price)
 
-cart_product_2 = driver_g.find_element(By.XPATH, "//a[@id='item_0_title_link']")
+cart_product_2 = driver_g.find_element(By.XPATH, "//a[@id='item_1_title_link']")
 value_cart_product_2 = cart_product_2.text
 print("Product 2 in cart is: " + value_cart_product_2)
 
-cart_product_2_price = driver_g.find_element(By.XPATH, '//*[@id="cart_contents_container"]/div/div[1]/div[4]/div[2]/div[2]/div')
+cart_product_2_price = driver_g.find_element(By.XPATH, "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div")
 value_cart_product_2_price = cart_product_2_price.text
 print("Costs: " + value_cart_product_2_price)
 
@@ -110,12 +106,43 @@ postal_code = driver_g.find_element(By.XPATH, "//input[@id='postal-code']")
 postal_code.send_keys("1337")
 print("Input postal code - success")
 
-time.sleep(2)
 continue_button = driver_g.find_element(By.XPATH, "//input[@id='continue']")
 continue_button.click()
 print("Go to checkout overview - success")
 
 # overview
+finish_product_1 = driver_g.find_element(By.XPATH, "//a[@id='item_0_title_link']")
+value_finish_product_1 = finish_product_1.text
+print("Product 1 in overview: " + value_finish_product_1)
 
+finish_price_product_1 = driver_g.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[1]/div[4]/div[2]/div[2]/div")
+value_finish_price_product_1 = finish_price_product_1.text
+print("Costs: " + value_finish_price_product_1)
 
+finish_product_2 = driver_g.find_element(By.XPATH, "//a[@id='item_1_title_link']")
+value_finish_product_2 = finish_product_2.text
+print("Product 2 in overview: " + value_finish_product_2)
 
+finish_price_product_2 = driver_g.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[1]/div[3]/div[2]/div[2]/div")
+value_finish_price_product_2 = finish_price_product_2.text
+print("Costs: " + value_finish_price_product_2)
+
+summary_price = driver_g.find_element(By.XPATH, "//div[@class='summary_subtotal_label']")
+value_summary_price = summary_price.text
+print(value_summary_price)
+item_total = value_summary_price.replace('Item total: $', '')
+items_sum = float(item_1) + float(item_2)
+
+# check
+assert value_product_1 == value_finish_product_1
+print("Product 1 in overview is the same as in cart")
+assert value_product_2 == value_finish_product_2
+print("Product 2 in overview is the same as in cart")
+assert value_product_1_price == value_finish_price_product_1
+print("Product 1 cost in overview is the same as in cart")
+assert value_product_1_price == value_finish_price_product_1
+print("Product 2 cost in overview is the same as in cart")
+assert float(item_total) == items_sum
+print("Summary price is correct")
+print("Smoke test - success!")
+driver_g.close()
